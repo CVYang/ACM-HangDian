5c9b95cb0c84a2010911986925cd6708767a1a77
CVYang <401506102@qq.com>
2019-09-04 00:41:22 +0800

    Add files via upload

--- CCF20181202/main.c
+++ CCF20181202/main.c
@@ -7,103 +7,96 @@ int main()
     int n = 0, sumt = 0,sum = 0;
     int kt[10001][2]={0};
     int RGlight[3]={0};
-    int i = 0, j = 0;
+    int i = 0, j = 0, l =0;
     scanf("%d %d %d",&RGlight[0],&RGlight[1],&RGlight[2]);
     scanf("%d",&n);
     for(i = 0; i < n; i++)
     {
         scanf("%d %d",&kt[i][0],&kt[i][1]);
     }
-    for(i = 0; i< n; i++)
+    for(j = 0; j < n; j++)
     {
-        if(kt[i][0] == 0)
+        if(kt[j][0] == 0)
         {
-            sumt += kt[i][1];
-            sum  = sumt;
-            /// update state
-            for(j = i+1; j < n; j ++)
+            sumt += kt[j][1];
+            sum = sumt;
+        }
+        if(kt[j][0] == 1)
+        {
+            kt[j][1] = sum%(RGlight[0]+RGlight[1]+RGlight[2]) - kt[j][1];
+            if(kt[j][1] < 0)
             {
-                if(kt[j][0] == 0)
-                {
-                    sum += kt[j][1];
-                }
-                if(kt[j][0] == 1)
-                {
-                    kt[j][1] = sum%(RGlight[0]+RGlight[1]+RGlight[2]) - kt[j][1];
-                    if(kt[j][1] < 0)
-                    {
-                        kt[j][1] *= -1;
-                    }
-                    else if(kt[j][1] < RGlight[2])
-                    {
-                        kt[j][0] = 3;
-                    }
-                    else if(kt[j][1] < (RGlight[2]+RGlight[1]))
-                    {
-                        kt[j][0] = 2;
-                        kt[j][1] = (RGlight[2]+RGlight[1]) - kt[j][1];
-                    }
-                    else if(kt[j][1] < (RGlight[0]+RGlight[1]+RGlight[2]))
-                    {
-                        kt[j][0] = 1;
-                        kt[j][1] = (RGlight[0]+RGlight[1]+RGlight[2]) - kt[j][1];
-                    }
-                }
-                else if(kt[j][0] == 2)
-                {
-                    kt[j][1] = sum%(RGlight[0]+RGlight[1]+RGlight[2]) - kt[j][1];
-                    if(kt[j][1] < 0)
-                    {
-                        kt[j][1] *= -1;
-                    }
-                    else if(kt[j][1] < RGlight[0])
-                    {
-                        kt[j][0] = 1;
-                        kt[j][1] = RGlight[0] - kt[j][1];
-                    }
-                    else if(kt[j][1] < (RGlight[0]+RGlight[2]))
-                    {
-                        kt[j][0] = 3;
-                        kt[j][1] = (RGlight[2]+RGlight[0]) - kt[j][1];
-                    }
-                    else if(kt[j][1] < (RGlight[0]+RGlight[1]+RGlight[2]))
-                    {
-                        kt[j][0] = 2;
-                        kt[j][1] = (RGlight[0]+RGlight[1]+RGlight[2]) - kt[j][1];
-                    }
-                }
-                else if(kt[j][0] == 3)
-                {
-                    kt[j][1] = sum%(RGlight[0]+RGlight[1]+RGlight[2]) - kt[j][1];
-                    if(kt[j][1] < 0)
-                    {
-                        kt[j][1] *= -1;
-                    }
-                    else if(kt[j][1] < RGlight[1])
-                    {
-                        kt[j][0] = 2;
-                        kt[j][1] = RGlight[1] - kt[i][1];
-                    }
-                    else if(kt[j][1] < (RGlight[0]+RGlight[1]))
-                    {
-                        kt[j][0] = 1;
-                        kt[j][1] = (RGlight[0]+RGlight[1]) - kt[j][1];
-                    }
-                    else if(kt[j][1] < (RGlight[0]+RGlight[1]+RGlight[2]))
-                    {
-                        kt[j][0] = 3;
-                        kt[j][1] = (RGlight[0]+RGlight[1]+RGlight[2]) - kt[j][1];
-                    }
-                }
+                kt[j][1] *= -1;
+            }
+            else if(kt[j][1] < RGlight[2])
+            {
+                kt[j][0] = 3;
+                kt[j][1] = RGlight[2] - kt[j][1];
+            }
+            else if(kt[j][1] < (RGlight[2]+RGlight[1]))
+            {
+                kt[j][0] = 2;
+                kt[j][1] = (RGlight[2]+RGlight[1]) - kt[j][1];
+            }
+            else if(kt[j][1] < (RGlight[0]+RGlight[1]+RGlight[2]))
+            {
+                kt[j][0] = 1;
+                kt[j][1] = (RGlight[0]+RGlight[1]+RGlight[2]) - kt[j][1];
             }
         }
-        printf("-------------  %d  --------------\n",i);
-        for(j = 0; j < n; j++)
+        else if(kt[j][0] == 2)
         {
-            printf("%d %d\n",kt[j][0],kt[j][1]);
+            kt[j][1] = sum%(RGlight[0]+RGlight[1]+RGlight[2]) - kt[j][1];
+            if(kt[j][1] < 0)
+            {
+                kt[j][1] *= -1;
+            }
+            else if(kt[j][1] < RGlight[0])
+            {
+                kt[j][0] = 1;
+                kt[j][1] = RGlight[0] - kt[j][1];
+            }
+            else if(kt[j][1] < (RGlight[0]+RGlight[2]))
+            {
+                kt[j][0] = 3;
+                kt[j][1] = (RGlight[2]+RGlight[0]) - kt[j][1];
+            }
+            else if(kt[j][1] < (RGlight[0]+RGlight[1]+RGlight[2]))
+            {
+                kt[j][0] = 2;
+                kt[j][1] = (RGlight[0]+RGlight[1]+RGlight[2]) - kt[j][1];
+            }
         }
-        if(kt[i][0] == 1)sumt += kt[i][1];
-        if(kt[i][0] == 2)sumt += (kt[i][1]+RGlight[0]);
+        else if(kt[j][0] == 3)
+        {
+            kt[j][1] = sum%(RGlight[0]+RGlight[1]+RGlight[2]) - kt[j][1];
+            if(kt[j][1] < 0)
+            {
+                kt[j][1] *= -1;
+            }
+            else if(kt[j][1] < RGlight[1])
+            {
+                kt[j][0] = 2;
+                kt[j][1] = RGlight[1] - kt[i][1];
+            }
+            else if(kt[j][1] < (RGlight[0]+RGlight[1]))
+            {
+                kt[j][0] = 1;
+                kt[j][1] = (RGlight[0]+RGlight[1]) - kt[j][1];
+            }
+            else if(kt[j][1] < (RGlight[0]+RGlight[1]+RGlight[2]))
+            {
+                kt[j][0] = 3;
+                kt[j][1] = (RGlight[0]+RGlight[1]+RGlight[2]) - kt[j][1];
+            }
+        }
+        if(kt[j][0] == 1)sumt += kt[j][1];
+        if(kt[j][0] == 2)sumt += (kt[j][1]+RGlight[0]);
+        printf("-------------  %d  --------------\n",i);
+        for(l = 0; l < n; l++)
+        {
+            printf("%d %d %d %d\n",kt[l][0],kt[l][1],sum,sumt);
+        }
     }
     printf("%d\n",sumt);
     sumt = 0;
